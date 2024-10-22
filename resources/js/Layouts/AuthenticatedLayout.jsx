494e27e65015d5f7e87ddeb52610baa56330d542
Jean Gouirand <elluc09@gmail.com>
2024-10-22 12:27:38 -0400

    arreglos del sidebar

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -18,6 +18,8 @@ export default function Authenticated({ user, header, children }) {
       return storedTheme || 'light';
    });
 
+   const [sidebarOpen, setSidebarOpen] = useState(false);
+
    useEffect(() => {
       if (theme === 'dark') {
          document.querySelector('html').classList.add('dark');
@@ -25,6 +27,11 @@ export default function Authenticated({ user, header, children }) {
          document.querySelector('html').classList.remove('dark');
       }
    }, [theme]);
+
+   const toggleSidebar = () => {
+      console.log('sidebar')
+      setSidebarOpen(!sidebarOpen); // Cambiar el estado del sidebar
+   };
 
    const toggleDarkMode = () => {
       setTheme(prevTheme => {
@@ -79,7 +86,14 @@ export default function Authenticated({ user, header, children }) {
             <div className="px-3 py-3 lg:px-5 lg:pl-3">
                <div className="flex items-center justify-between">
                   <div className="flex items-center justify-start rtl:justify-end">
-                     <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                     <button
+                        data-drawer-target="logo-sidebar"
+                        data-drawer-toggle="logo-sidebar"
+                        aria-controls="logo-sidebar"
+                        type="button"
+                        className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        onClick={toggleSidebar}
+                     >
                         <span className="sr-only">Open sidebar</span>
                         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                            <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
@@ -139,7 +153,11 @@ export default function Authenticated({ user, header, children }) {
             </div>
          </nav>
 
-         <aside id="logo-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
+         <aside
+            id="logo-sidebar"
+            className={`fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'} sm:translate-x-0 bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700`} 
+            
+            aria-label="Sidebar">
             <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
                <ul className="space-y-2 font-medium">
                   <li>
@@ -230,7 +248,7 @@ export default function Authenticated({ user, header, children }) {
             <footer className='flex flex-col justify-end w-auto mt-auto border-t dark:border-t-gray-700'>
                <div className="flex justify-center p-4 text-gray-800 dark:text-gray-200">
 
-               <p>© 2024 <Link href="#">Knots Agency</Link>. All rights reserved.</p>
+                  <p>© 2024 <Link href="#">Knots Agency</Link>. All rights reserved.</p>
                </div>
             </footer>
          </div>
